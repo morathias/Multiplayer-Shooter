bb4bf883db9d83b27009e95eaaebdc477c4f460e
morathias <matias_morales_1996@hotmail.com>
2017-10-18 22:26:12 -0300

    Primer Commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Player : NetworkBehaviour {
+
+    public GameObject arrow;
+
+    public float rotateSpeed;
+    public float movingSpeed;
+
+    float _life = 100f;
+    float _currentLife;
+    float _angle;
+
+    Vector3 _mousePosition;
+    Vector3 _position;
+
+	void Update () {
+        if (!isLocalPlayer)
+            return;
+
+        _currentLife = _life;
+
+        rotate();
+
+        Vector3 translate = new Vector3(Input.GetAxis("Horizontal"),
+                                        0,
+                                        Input.GetAxis("Vertical"));
+
+        transform.Translate(translate * movingSpeed * Time.deltaTime, Space.World);
+
+        if (Input.GetButtonDown("Fire1"))
+            Cmdfire();
+	}
+
+    [Command]
+    void Cmdfire() {
+        NetworkServer.Spawn(Instantiate(arrow, transform.position + (transform.forward * 0.5f + new Vector3(0, 0.5f, 0)), transform.rotation));
+    }
+
+    public override void OnStartLocalPlayer(){
+        transform.GetChild(0).GetComponent<MeshRenderer>().material.color = Color.blue;
+    }
+
+    public void applyDamage(float damageToApply) {
+        _currentLife -= damageToApply;
+    }
+
+    void rotate()
+    {
+        _mousePosition = Input.mousePosition;    //agarro la posicion del mouse
+        _mousePosition.z = 13f;  //distancia de la camara al prota
+
+        _position = Camera.main.WorldToScreenPoint(transform.position);    //convierto la posicion del prota a screen
+
+        _mousePosition.x -= _position.x;    //saco la direccion
+        _mousePosition.y -= _position.y;    //_|
+
+        _angle = Mathf.Atan2(_mousePosition.x, _mousePosition.y) * Mathf.Rad2Deg;    //saco el angulo de ese vector
+        transform.rotation = Quaternion.Euler(new Vector3(0, _angle, 0));      //lo roto en eje y
+    }
+}
---
+++ Assets/Scripts/Weapons/Arrow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Arrow : NetworkBehaviour {
+
+    float _lifeTime = 3f;
+
+	void Update () {
+        _lifeTime -= Time.deltaTime;
+
+        if (_lifeTime <= 0)
+            Destroy(gameObject);
+
+        transform.Translate(transform.forward  * 10f * Time.deltaTime, Space.World);
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "Player")
+            Destroy(gameObject);
+    }
+}
